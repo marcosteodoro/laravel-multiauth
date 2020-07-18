5ef62e53dfaf9c20289b0ff3c9248823906dd378
Marcos Teodoro <marcosvinici_us@live.com>
2020-07-18 01:34:37 -0300

    Multi-auth setup with Laravel auth scaffold

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,6 +39,27 @@ class Handler extends ExceptionHandler
     public function report(Throwable $exception)
     {
         parent::report($exception);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        if ($request->is('admin') || $request->is('admin/*')) {
+            return redirect()->guest('/login/admin');
+        }
+        if ($request->is('writer') || $request->is('writer/*')) {
+            return redirect()->guest('/login/writer');
+        }
+        return redirect()->guest(route('login'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
+Route::view('/', 'welcome');
+Auth::routes();
 
-use Illuminate\Support\Facades\Route;
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
+Route::get('/login/writer', 'Auth\LoginController@showWriterLoginForm');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+Route::get('/register/writer', 'Auth\RegisterController@showWriterRegisterForm');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/writer', 'Auth\LoginController@writerLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
+Route::post('/register/writer', 'Auth\RegisterController@createWriter');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/home', 'home')->middleware('auth');
+Route::view('/admin', 'admin');
+Route::view('/writer', 'writer');
